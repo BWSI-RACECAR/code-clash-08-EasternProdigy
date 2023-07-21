eccafcae9cf9040673214d641668d57e831f7422
William Mezitis <97573439+EasternProdigy@users.noreply.github.com>
2023-07-21 09:13:47 -0400

    Add files via upload

--- optimizescan.py
+++ optimizescan.py
@@ -34,10 +34,7 @@ Input: 404 ; Output: 0.33
 
 class Solution:    
     def optimizescan(self, battcap):
-        distance = battcap/250
-        distance1 = distance / 6
-        side = distance/4
-        return side * (side * 2)
+        return (battcap/250)/4 * ((battcap/250)/4 * 2)
 
         #TODO: Write code below to return a float with the solution to the prompt.
 
