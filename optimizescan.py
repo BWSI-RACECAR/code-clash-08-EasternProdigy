d9767e2abfbebeb92d76545ef1b500826e980484
William Mezitis <97573439+EasternProdigy@users.noreply.github.com>
2023-08-03 23:51:39 -0400

    Add files via upload

--- optimizescan.py
+++ optimizescan.py
@@ -34,7 +34,7 @@ Input: 404 ; Output: 0.33
 
 class Solution:    
     def optimizescan(self, battcap):
-        return (battcap/250)/4 * ((battcap/250)/4 * 2)
+        return ((battcap / 250) / 4)**2
 
         #TODO: Write code below to return a float with the solution to the prompt.
 
